d1744875e7d1a814962ee279d0412f99a2e756cb
alizainalabidin01 <mohbagir4@gmail.com>
2022-04-11 07:14:38 +0700

    twelev commit

--- pages/input.js
+++ pages/input.js
@@ -1,16 +1,53 @@
 import { useState } from "react";
-
 import axios from "axios";
-import { useRouter } from "next/router";
+import dbConnect from "../utils/mongo";
+import bodyParser from "body-parser";
+import { promisify } from "util";
+import products from "../models/product";
 
 
-const Add = () => {
+const getBody = promisify(bodyParser.urlencoded());
+
+export const getServerSideProps = async ({ req, res}) => {
+  await dbConnect();
+
+  if (req.method === "POST") {
+   await getBody(req, res);
+   const img = req.body.imgUrl
+   const desc = req.body.desc
+   const price0 = req.body.price0
+   const price1 = req.body.price1
+   const price2 = req.body.price2
+   console.log(price1);
+   const type = req.body.type
+   const name_product = req.body.name_product
+   const newProduct = ({
+            img,
+            desc,
+            price:[price0,price1,price2],
+            type,
+            name_product
+          })
+   await products.create( newProduct)
+    
+  }
+    return {
+      props : {
+      
+      }
+    };
+
+}
+
+function Add ({props}){
   const [img, setImg] = useState([]);
   const [imgUrl, setImgUrl] = useState([]);
   const [name_product, setNameProduct] = useState(null);
   const [type, setType] = useState(null);
   const [desc, setDesc] = useState(null);
-  const [price, setPrices] = useState([]);
+  const [price, setPrices] = useState([Number]);
+  const [new1, setNew] = useState([]);
+  props=new1
   // const [extraCategory, setExtraCategory] = useState([]);
   // const [extra, setExtra] = useState(null);
 
@@ -47,25 +84,25 @@ const Add = () => {
     }
   }
 
-  const handleCreate = async () => {
-      const newProduct = {
-        name_product,
-        desc,
-        price,
-        type,
-        // extraCategory,
-        img: imgUrl,
-      };
+  // const handleCreate = async () => {
+  //     const newProduct = {
+  //       name_product,
+  //       desc,
+  //       price,
+  //       type,
+  //       // extraCategory,
+  //       img: imgUrl,
+  //     };
 
-      await axios.post("http://localhost:3000/api", newProduct);
-      setImg(''),
-      setImgUrl(''),
-      setDesc(''),
-      setNameProduct(''),
-      setPrices(''),
-      setType('')
-      alert("data berhasil dimasukan")
-  };
+  //     await axios.post("http://localhost:3000/api", newProduct);
+  //     setImg(''),
+  //     setImgUrl(''),
+  //     setDesc(''),
+  //     setNameProduct(''),
+  //     setPrices(''),
+  //     setType('')
+  //     alert("data berhasil dimasukan")
+  // };
 
   return (
     <div >
@@ -154,9 +191,19 @@ const Add = () => {
             ))}
           </div> */}
         </div>
-        <button  onClick={handleCreate} className="bg-[#46391e] px-1 py-1 text-white  hover:bg-[#f5eddc] hover:text-black">
+        <form method="POST" >
+
+        <button  type="submit" className="bg-[#46391e] px-1 py-1 text-white  hover:bg-[#f5eddc] hover:text-black">
           Create
         </button>
+        <input className="opacity-0" name="name_product" defaultValue={props.name_product = name_product}  readOnly="readonly"/>
+        <input className="opacity-0" name="desc" defaultValue={props.desc = desc}  readOnly="readonly"/>
+        <input className="opacity-0" name="price0" defaultValue={props.price0 = price[0]}  readOnly="readonly"/>
+        <input className="opacity-0" name="price1" defaultValue={props.price1 = price[1]}  readOnly="readonly"/>
+        <input className="opacity-0" name="price2" defaultValue={props.price2 = price[2]}  readOnly="readonly"/>
+        <input className="opacity-0" name="type" defaultValue={props.type = type}  readOnly="readonly"/>
+        <input className="opacity-0" name="imgUrl" defaultValue={props.imgUrl = imgUrl}  readOnly="readonly"/>
+        </form>
       </div>
     </div>
   );
