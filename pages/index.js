c13f03c90daa44a899918acad1a2f1e140686215
nawawis <nawawisboutique@gmail.com>
2022-06-02 19:43:22 +0700

    Added my project

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({datanew, databest, dataall}) {
             <Link href={'/detail/' + item._id} key={item._id}>
               <div className="flex flex-col gap-2 p-1 my-1 font-['sans-serif'] text-xs bg-white hover:bg-[#f5eddc]">
                 <div className="flex flex-col gap-2">
-                  <img className="object-contain" src={item.img} />
+                  <img className="object-contain" src={item.img[0]} />
                   <p>{item.name_product.substring(0,26)+'...'}</p>
                   <p>like :{item.like}</p>
                   
@@ -40,7 +40,7 @@ export default function Home({datanew, databest, dataall}) {
             <Link href={'/detail/' +item._id} key={item._id}><a>
               <div className="flex flex-col gap-2 p-1 my-1 font-['sans-serif'] text-xs bg-white hover:bg-[#f5eddc]">
                 <div className="flex flex-col gap-2">
-                  <img className="object-contain" src={item.img} />
+                  <img className="object-contain" src={item.img[0]} />
                   <p>{item.name_product.substring(0,26)+'...'}</p>
                   <p>like :{item.like}</p>
                   <div className=" flex flex-row w-min  my-2 rounded-xl text-xs ">{"Rp."+item.price[0].toLocaleString()}
