d3da85f313cda7c6eff629df04d969952df02146
nawawis <nawawisboutique@gmail.com>
2022-07-01 11:09:27 +0700

    upload 26

--- pages/payment/[_id].js
+++ pages/payment/[_id].js
@@ -167,17 +167,15 @@ function Details ({dataorder, props}) {
         
         </div>
         
-        {/* <div className="bg-fuchsia-100 p-6 mt-3">
+        <div className="bg-fuchsia-100 p-6 mt-3 font-normal text-xs">
         <h2 className="font-semibold mb-2">Metode Pembayaran</h2>
-        <p className="font-normal text-xs">
-          - CIMB NIAGA : 706926281100  A/N TUTUT IRYANA DEVI <br/>
-          - BRI : 614301019224536  A/N TUTUT IRYANA DEVI <br/>
-          - BTPN : 90200194182  A/N TUTUT IRYANA DEVI <br/> 
-          - DANA : 081354181920  A/N TUTUT IRYANA DEVI <br/>
-          - SHOPEEPAY : 088989319606  A/N nawawi's.boutique <br/><br/>
-          Note: Harap pastikan nomor dan nama penerima sesuai dengan yang tertera diatas.
-        </p>
-        </div> */}
+        <p>- CIMB NIAGA : 706926281100  A/N TUTUT IRYANA DEVI</p>
+        <p> - BRI : 614301019224536  A/N TUTUT IRYANA DEVI </p>
+        <p>- BTPN : 90200194182  A/N TUTUT IRYANA DEVI </p> 
+        <p>- DANA : 081354181920  A/N TUTUT IRYANA DEVI </p>
+        <p> - SHOPEEPAY : 088989319606  A/N nawawi's.boutique </p> <br/>
+        <p> Note: Harap pastikan nomor dan nama penerima sesuai dengan yang tertera diatas.</p>
+        </div>
         
         </section>
         
